841054b4038c30d7253edb5d6f55bc76efd59c72
Julia Rebello <julia.rebello@ibm.com>
2021-06-21 15:18:43 -0500

    fixing styling issues

--- tests/unit/plugins/modules/test_flrtvc.py
+++ tests/unit/plugins/modules/test_flrtvc.py
@@ -7,34 +7,33 @@ __metaclass__ = type
 
 import unittest
 from unittest import mock
-import copy
 import re
 
 from ansible_collections.ibm.power_aix.plugins.modules import flrtvc
 
 from .common.utils import (
-    rootdir, mock_ftp_report, mock_http_report, mock_https_report
+    mock_ftp_report, mock_http_report, mock_https_report
 )
 
 params = {
-        'apar': None,
-        'filesets': None,
-        'csv': None,
-        'path': '/var/adm/ansible',
-        'save_report': False,
-        'verbose': False,
-        'force': False,
-        'clean': False,
-        'check_only': False,
-        'download_only':False,
-        'extend_fs':True,
-        'protocol': None,
+    'apar': None,
+    'filesets': None,
+    'csv': None,
+    'path': '/var/adm/ansible',
+    'save_report': False,
+    'verbose': False,
+    'force': False,
+    'clean': False,
+    'check_only': False,
+    'download_only': False,
+    'extend_fs': True,
+    'protocol': None,
 }
 
 init_results = {
     'changed': False,
     'msg': '',
-    'meta':{'messages': []}
+    'meta': {'messages': []}
     # meta structure will be updated as follow:
     # meta={'messages': [],     detail execution messages
     #       '0.report': [],     run_flrtvc reports the vulnerabilities
@@ -53,18 +52,18 @@ mock_http = []
 mock_ftp = []
 
 
-#this function will check that each entry in the list of urls has the correct protocol
-#it is used to check the "1.parse" part of "meta" in the report for a protocol.
-#this will determine whether the changes to the code worked as intended.
+# this function will check that each entry in the list of urls has the correct protocol
+# it is used to check the "1.parse" part of "meta" in the report for a protocol.
+# this will determine whether the changes to the code worked as intended.
 def _check_protocol(subs, s_lst):
     for url in s_lst:
         # the colon is important as to not match http in https
         pattern = r"^%s:" % subs
         found = re.search(pattern, url)
         if not found:
-            #return false if even one of the urls does not have the correct protocol
+            # return false if even one of the urls does not have the correct protocol
             return False
-        #return true if all urls in the list use the specified protocol
+        # return true if all urls in the list use the specified protocol
         return True
 
 
@@ -76,7 +75,7 @@ class TestRunParser(unittest.TestCase):
         self.module.debug.return_value = None
         self.results = init_results
 
-        #mocks of what the flrtvc script returns
+        # mocks of what the flrtvc script returns
         with open(mock_https_report, "r") as mock_file:
             mock_https = mock_file.read()
             mock_https = mock_https.split(",")
@@ -94,31 +93,31 @@ class TestRunParser(unittest.TestCase):
         flrtvc.module = self.module
         flrtvc.results = self.results
         flrtvc.results['meta'].update({'0.report': mock_http})
-        flrtvc.run_parser(flrtvc.results['meta']['0.report'])  
-        self.assertTrue(_check_protocol('ftp', flrtvc.results['meta']['1.parse']))    
+        flrtvc.run_parser(flrtvc.results['meta']['0.report'])
+        self.assertTrue(_check_protocol('ftp', flrtvc.results['meta']['1.parse']))
 
     def test_http_to_https(self):
         self.module.params['protocol'] = "https"
         flrtvc.module = self.module
         flrtvc.results = self.results
         flrtvc.results['meta'].update({'0.report': mock_http})
-        flrtvc.run_parser(flrtvc.results['meta']['0.report'])  
-        self.assertTrue(_check_protocol('https', flrtvc.results['meta']['1.parse']))  
+        flrtvc.run_parser(flrtvc.results['meta']['0.report'])
+        self.assertTrue(_check_protocol('https', flrtvc.results['meta']['1.parse']))
 
     def test_ftp_to_http(self):
         self.module.params['protocol'] = "http"
         flrtvc.module = self.module
         flrtvc.results = self.results
         flrtvc.results['meta'].update({'0.report': mock_ftp})
-        flrtvc.run_parser(flrtvc.results['meta']['0.report'])  
-        self.assertTrue(_check_protocol('http', flrtvc.results['meta']['1.parse'])) 
+        flrtvc.run_parser(flrtvc.results['meta']['0.report'])
+        self.assertTrue(_check_protocol('http', flrtvc.results['meta']['1.parse']))
 
     def test_ftp_to_https(self):
         self.module.params['protocol'] = "https"
         flrtvc.module = self.module
         flrtvc.results = self.results
         flrtvc.results['meta'].update({'0.report': mock_ftp})
-        flrtvc.run_parser(flrtvc.results['meta']['0.report'])  
+        flrtvc.run_parser(flrtvc.results['meta']['0.report'])
         self.assertTrue(_check_protocol('https', flrtvc.results['meta']['1.parse']))
 
     def test_https_to_ftp(self):
@@ -126,13 +125,13 @@ class TestRunParser(unittest.TestCase):
         flrtvc.module = self.module
         flrtvc.results = self.results
         flrtvc.results['meta'].update({'0.report': mock_https})
-        flrtvc.run_parser(flrtvc.results['meta']['0.report'])  
-        self.assertTrue(_check_protocol('ftp', flrtvc.results['meta']['1.parse']))  
+        flrtvc.run_parser(flrtvc.results['meta']['0.report'])
+        self.assertTrue(_check_protocol('ftp', flrtvc.results['meta']['1.parse']))
 
     def test_https_to_http(self):
         self.module.params['protocol'] = "http"
         flrtvc.module = self.module
         flrtvc.results = self.results
         flrtvc.results['meta'].update({'0.report': mock_https})
-        flrtvc.run_parser(flrtvc.results['meta']['0.report'])  
-        self.assertTrue(_check_protocol('http', flrtvc.results['meta']['1.parse']))  
+        flrtvc.run_parser(flrtvc.results['meta']['0.report'])
+        self.assertTrue(_check_protocol('http', flrtvc.results['meta']['1.parse']))
